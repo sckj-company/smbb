a595f612303a81c974335d96f8d7e337f9756eb7
Makene Neto <makeneto17@gmail.com>
2026-09-04 15:39:19 +0100

    feat: adjust Navbar and Footer rendering based on the admin route

--- src/components/SiteShell.tsx
+++ src/components/SiteShell.tsx
@@ -1,13 +1,21 @@
+"use client";
+
 import React from "react";
+import { usePathname } from "next/navigation";
 import Navbar from "./Navbar";
 import ScrollToTop from "./ScrollToTop";
+import Footer from "./Footer";
 
 export default function SiteShell({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const isAdminRoute = pathname.startsWith("/admin");
+
   return (
     <React.Fragment>
       <ScrollToTop />
-      <Navbar />
+      {!isAdminRoute && <Navbar  />}
       {children}
+      {!isAdminRoute && <Footer />}
     </React.Fragment>
   );
 }
