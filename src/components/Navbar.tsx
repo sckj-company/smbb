41c343ce2dcf68ec4b8a1162ffb6f1a534a29554
Makene Neto <makeneto17@gmail.com>
2026-09-04 15:38:58 +0100

    feat: refatorar componente Navbar para ajustar estilos de layout e responsividade

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,8 +11,8 @@ export default function Navbar() {
   const { handleLanguageChange, selectedLanguage } = useTranslate();
 
   return (
-    <nav className="px-4 lg:py-2.5 xl:py-4 md:px-6">
-      <div className="mx-auto flex max-w-7xl items-center justify-between gap-4">
+    <nav className="fixed top-0  left-1/2 -translate-x-1/2 z-10 w-full max-w-7xl bg-white py-5">
+      <div className="flex md:w-5xl 2xl:w-7xl mx-auto items-center justify-between gap-4">
         <div className="flex items-center gap-5.5">
           <Logo />
           <div className="w-px h-6 border-l border-blue-100" />
