b80c87b89a989985f4618e157592bce01900271f
Makene Neto <makeneto17@gmail.com>
2026-09-07 17:39:13 +0100

    feat: refactors the AdminLayout component to improve code readability

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,12 +1,14 @@
 import AdminNavbar from "@/components/admin/AdminNavbar";
 
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
+export default function AdminLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
       <AdminNavbar />
-      <div className="min-h-screen px-4 pb-12 pt-32 md:px-6">
-        {children}
-      </div>
+      <div className="min-h-screen px-4 pb-12 pt-32 md:px-6">{children}</div>
     </>
   );
 }
