f0e4af41eee5feb56afff850dc5cbdec62c00a01
Makene Neto <makeneto17@gmail.com>
2026-09-07 17:40:20 +0100

    feat: refactor the login page to include a layout with a logo and description

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,9 +1,31 @@
+import { Suspense } from "react";
 import { LoginForm } from "@/components/auth/LoginForm";
+import Image from "next/image";
 
 export default function LoginPage() {
   return (
     <main className="login-page">
-      <LoginForm />
+      <Suspense fallback={null}>
+        <section className="login-container">
+          <div className="login-fire flex flex-col justify-between p-4 text-white">
+            <Image
+              src="/smbb-logo-dark.webp"
+              alt="SMBB Logo"
+              width="200"
+              height="100"
+              className="sm:h-6.5 xl:h-7.5 w-fit"
+            />
+
+            <div>
+              <h1 className="text-xl font-semibold">
+                SMBB - Comércio e Serviços, (SU), LDA
+              </h1>
+              <p className="text-sm text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+          </div>
+          <LoginForm />
+        </section>
+      </Suspense>
     </main>
   );
 }
