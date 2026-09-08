ae3504608963b86dccc8e56f315f01c7e6cc222a
Makene Neto <makeneto17@gmail.com>
2026-09-08 17:47:45 +0100

    fix: simplificar o componente Footer e ajustar estilos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,97 +1,25 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import Logo from "./Logo"
-import { useTranslation } from "react-i18next"
+import Logo from "./Logo";
+import { useTranslation } from "react-i18next";
 
 export default function Footer() {
-  const { t } = useTranslation()
-  const now = new Date()
-  const currentYear = now.getFullYear()
+  const { t } = useTranslation();
+  const now = new Date();
+  const currentYear = now.getFullYear();
 
   return (
-    <footer className="md:w-5xl 2xl:w-7xl mx-auto mt-50 grid gap-30 pb-4">
-      <article className="grid md:flex justify-between">
-        <div className="grid gap-5 w-120">
-          <Logo />
-          <p className="text-gray-600">{t("footer.description")}</p>
-        </div>
+    <footer className="md:w-5xl 2xl:w-7xl mx-auto mt-30 sm:mt-50 grid gap-15 pb-4 px-4 sm:px-0">
+      <div className="grid gap-5 2x:w-120">
+        <Logo />
+        <p className="text-sm sm:text-base text-gray-600">
+          {t("footer.description")}
+        </p>
+      </div>
 
-        <div className="flex gap-20">
-          <div className="space-y-3">
-            <h2 className="font-semibold text-blue-950">Explore</h2>
-            <ul className="grid gap-1.5">
-              <Link
-                href="/"
-                className="text-gray-600 transition-colors hover:underline hover:text-blue-900"
-              >
-                Home
-              </Link>
-              <Link
-                href="/"
-                className="text-gray-600 transition-colors hover:underline hover:text-blue-900"
-              >
-                Produtos
-              </Link>
-              <Link
-                href="/"
-                className="text-gray-600 transition-colors hover:underline hover:text-blue-900"
-              >
-                Serviços
-              </Link>
-            </ul>
-          </div>
-          <div className="space-y-3">
-            <h2 className="font-semibold text-blue-950">Serviços</h2>
-            <ul className="grid gap-1.5">
-              <Link
-                href="/"
-                className="text-gray-600 transition-colors hover:underline hover:text-blue-900"
-              >
-                Home
-              </Link>
-              <Link
-                href="/"
-                className="text-gray-600 transition-colors hover:underline hover:text-blue-900"
-              >
-                Produtos
-              </Link>
-              <Link
-                href="/"
-                className="text-gray-600 transition-colors hover:underline hover:text-blue-900"
-              >
-                Serviços
-              </Link>
-            </ul>
-          </div>
-          <div className="space-y-3">
-            <h2 className="font-semibold text-blue-950">Siga-nos</h2>
-            <ul className="grid gap-1.5">
-              <Link
-                href="/"
-                className="text-gray-600 transition-colors hover:underline hover:text-blue-900"
-              >
-                Home
-              </Link>
-              <Link
-                href="/"
-                className="text-gray-600 transition-colors hover:underline hover:text-blue-900"
-              >
-                Produtos
-              </Link>
-              <Link
-                href="/"
-                className="text-gray-600 transition-colors hover:underline hover:text-blue-900"
-              >
-                Serviços
-              </Link>
-            </ul>
-          </div>
-        </div>
-      </article>
-      <p className="text-gray-600">
+      <p className="text-sm sm:text-base">
         © {currentYear} {t("footer.copyright")}
       </p>
     </footer>
-  )
+  );
 }
