df3c8d7653ebb0d1d1e49504fd93a71c153a90ea
Makene Neto <makeneto17@gmail.com>
2026-09-08 13:12:45 +0100

    feat: alterar atributo lang para 'zh' no layout principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
+      lang="zh"
       className={cn(
         "h-full",
         "antialiased",
