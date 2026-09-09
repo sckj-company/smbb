9a129a2f0471bdeb9c879267b0b14e04e01483d4
Makene Neto <makeneto17@gmail.com>
2026-09-09 07:41:09 +0100

    style: aplicar responsividade no footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,15 +9,15 @@ export default function Footer() {
   const currentYear = now.getFullYear();
 
   return (
-    <footer className="md:w-5xl 2xl:w-7xl mx-auto mt-30 sm:mt-50 grid gap-15 pb-4 px-4 sm:px-0">
-      <div className="grid gap-5 2x:w-120">
+    <footer className="md:w-5xl 2xl:w-7xl mx-auto mt-30 sm:mt-50 grid xl:flex xl:justify-between gap-15 pb-4 xl:pb-8 px-4 sm:px-0">
+      <div className="grid gap-5 xl:w-120">
         <Logo />
-        <p className="text-sm sm:text-base text-gray-600">
+        <p className="text-sm 2xl:text-base text-gray-600">
           {t("footer.description")}
         </p>
       </div>
 
-      <p className="text-sm sm:text-base">
+      <p className="text-sm 2xl:text-base self-end">
         © {currentYear} {t("footer.copyright")}
       </p>
     </footer>
