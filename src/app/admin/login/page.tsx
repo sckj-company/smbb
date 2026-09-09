6ed49274cea96dbb300cae69b55f1b82926095c6
Makene Neto <makeneto17@gmail.com>
2026-09-09 09:07:12 +0100

    style: aplicar responsividade na página de login

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,6 +1,5 @@
 import { Suspense } from "react";
 import { LoginForm } from "@/components/auth/LoginForm";
-import Image from "next/image";
 
 export default function LoginPage() {
   return (
@@ -8,19 +7,15 @@ export default function LoginPage() {
       <Suspense fallback={null}>
         <section className="login-container">
           <div className="login-fire flex flex-col justify-between p-4 text-white">
-            <Image
-              src="/smbb-logo-dark-transparent.png"
-              alt="SMBB Logo"
-              width="200"
-              height="100"
-              className="h-8 sm:h-6.5 xl:h-7.5 w-fit"
-            />
+            <div />
 
             <div>
               <h1 className="2xl:text-xl font-semibold">
                 SMBB - Comércio e Serviços, (SU), LDA
               </h1>
-              <p className="text-sm text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <p className="text-sm text-gray-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
             </div>
           </div>
 
