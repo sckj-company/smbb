ab4f78e890722682c19f35299cc7e3b78ab76012
Makene Neto <makeneto17@gmail.com>
2026-09-09 15:09:28 +0100

    feat: atualizar componentes de botão na seção HeroButtons

--- src/components/home/ui/hero-buttons.tsx
+++ src/components/home/ui/hero-buttons.tsx
@@ -4,18 +4,20 @@ import { Button } from "@/components/ui/button";
 
 export default function HeroButtons() {
   return (
-    <div className="flex flex-col items-center gap-4 sm:flex-row">
-      <button className="cursor-pointer mt-6 rounded-lg bg-sky-500 dark:bg-sky-950 px-4 py-1.5 text-white transition-colors hover:bg-sky-600 hover:dark:bg-sky-800">
-        Become a supporter
-      </button>
+    <div className="flex items-center gap-4 sm:flex-row">
+      <Button
+        size="lg"
+        className="cursor-pointer mt-6 rounded-lg bg-sky-500 dark:bg-sky-950 px-4 py-1.5 text-white transition-colors hover:bg-sky-600 hover:dark:bg-sky-800"
+      >
+        Ver produtos
+      </Button>
 
-      <Button variant="ghost">
-        <Link
-          href="#benefits"
-          className="cursor-pointer mt-6 rounded-lg px-4 py-1.5 text-black dark:text-white transition-colors"
-        >
-          Read more
-        </Link>
+      <Button
+        size="lg"
+        variant="ghost"
+        className="cursor-pointer mt-6 rounded-lg px-4 py-1.5 text-black dark:text-white transition-colors"
+      >
+        <Link href="#benefits">Ver Serviços</Link>
       </Button>
     </div>
   );
