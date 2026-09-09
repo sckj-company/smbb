3f671136716e300295add58b6a2c6910ca181bac
Makene Neto <makeneto17@gmail.com>
2026-09-09 14:53:56 +0100

    feat: adicionar componente HomeHero

---
+++ src/components/home/HomeHero.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import HeroButtons from "./ui/hero-buttons";
+import HeroContent from "./ui/hero-content";
+
+export default function HomeHero() {
+  return (
+    <header className="mt-25 mb-60 flex flex-col items-center justify-center gap-4 text-center">
+      <HeroContent />
+      <HeroButtons />
+    </header>
+  );
+}
