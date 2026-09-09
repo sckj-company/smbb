eb8b5e9d32b8a49d116c448a44dab4c80f149b5d
Makene Neto <makeneto17@gmail.com>
2026-09-09 14:53:49 +0100

    feat: adicionar componente HomeContent

---
+++ src/components/home/HomeContent.tsx
@@ -0,0 +1,11 @@
+import HomeHero from "./HomeHero";
+import MobileCheckout from "./MobileCheckout";
+
+export default function HomeContent() {
+  return (
+    <main>
+      <HomeHero />
+      <MobileCheckout />
+    </main>
+  );
+}
