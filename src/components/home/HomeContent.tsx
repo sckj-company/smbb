57d89886ce40a3dbaeabce73da0628b855b1e24c
Makene Neto <makeneto17@gmail.com>
2026-09-09 15:08:52 +0100

    feat: remover componente MobileCheckout de HomeContent

--- src/components/home/HomeContent.tsx
+++ src/components/home/HomeContent.tsx
@@ -1,11 +1,9 @@
 import HomeHero from "./HomeHero";
-import MobileCheckout from "./MobileCheckout";
 
 export default function HomeContent() {
   return (
     <main>
       <HomeHero />
-      <MobileCheckout />
     </main>
-  );
+  )
 }
