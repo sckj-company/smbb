24b621817517ef12bfa24f30f0309aff283804d3
Makene Neto <makeneto17@gmail.com>
2026-09-11 12:01:07 +0100

    feat: reorganizar componentes na página inicial para melhor estrutura

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import CTA from "@/components/CTA";
 import { Faq } from "@/components/Faq";
 import HomeHero from "@/components/home/HomeHero";
 import HowItWorksSection from "@/components/HowItWorksSection";
+import Numbers from "@/components/Numbers";
 import Service from "@/components/Service";
 
 export default function HomePage() {
@@ -9,8 +10,9 @@ export default function HomePage() {
     <main className=" bg-dotted mt-30 sm:mt-35 xl:mt-40 2xl:mt-10 w-full pb-12 px-4 sm:px-8 2xl:px-0 ">
       <div>
         <HomeHero />
-        <Service />
+        <Numbers />
         <HowItWorksSection />
+        <Service />
         <Faq />
         <CTA />
       </div>
