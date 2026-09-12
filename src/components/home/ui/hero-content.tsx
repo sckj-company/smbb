316ac05333eb844e5279b57818e36c52af116eeb
Makene Neto <makeneto17@gmail.com>
2026-09-12 11:40:25 +0100

    feat: adicionar conteúdo dinâmico ao componente HeroContent

--- src/components/home/ui/hero-content.tsx
+++ src/components/home/ui/hero-content.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Availability from "@/components/Availability";
+import { useTranslation } from "react-i18next";
 
 export default function HeroContent() {
+  const { t } = useTranslation();
+
   return (
-    
+    <div className="max-w-164 text-center md:text-left">
+      <Availability />
+      <h1 className="mt-8 text-4xl font-bold tracking-tighter sm:text-6xl md:text-[4.2rem]">
+        {t("header.title")}
+      </h1>
+
+      <p className="mx-auto mt-8 max-w-140 text-black/65 md:mx-0">
+        {t("header.description")}
+      </p>
+    </div>
   );
 }
