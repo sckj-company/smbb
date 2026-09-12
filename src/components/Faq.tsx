8ca83c647a4a0473a8baac7171a41218e8e2389a
Makene Neto <makeneto17@gmail.com>
2026-09-12 11:53:43 +0100

    fix: ajustar largura do contêiner na seção de FAQ

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -8,7 +8,7 @@ export function Faq() {
 
   return (
     <section className="border-y border-blue-200 bg-sky-50 py-16 sm:py-24 px-6 sm:px-0">
-      <div className="mx-auto grid w-full max-w-7xl gap-8 sm:px-6 md:grid-cols-[minmax(0,1fr)_minmax(0,1.3fr)] md:items-start md:gap-10 lg:gap-20 xl:px-0">
+      <div className="md:w-5xl 2xl:w-7xl mx-auto grid w-full max-w-7xl gap-8 sm:px-6 md:grid-cols-[minmax(0,1fr)_minmax(0,1.3fr)] md:items-start md:gap-10 lg:gap-20 xl:px-0">
         <section className="w-full space-y-3 md:max-w-sm">
           <h1 className="text-3xl font-bold text-sky-900 sm:text-4xl">
             {t("faq.title")}
