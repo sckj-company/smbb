f6ec70c565d8a8899ce8c624e65f120bb24cd064
Makene Neto <makeneto17@gmail.com>
2026-09-12 11:39:47 +0100

    feat: atualizar componente Availability para usar tradução e melhorar estilo

--- src/components/Availability.tsx
+++ src/components/Availability.tsx
@@ -1,12 +1,18 @@
 import useAvailability from "@/hooks/useAvailability";
+import { useTranslation } from "react-i18next";
 
 export default function Availability() {
+  const { t } = useTranslation();
   const availability = useAvailability();
 
   return (
-    <div className="self-end mt-10 h-fit w-fit bg-red-200 rounded-[6rem] flex items-center gap-1 py-1 px-2">
-      <div className="inline-flex bg-red-500 w-2 h-2 rounded-full animate-pulse" />
-      <h1 className="text-sm font-medium">{availability}</h1>
+    <div
+      className={`mx-auto mt-10 flex h-fit w-fit items-center gap-1 rounded-[6rem] border px-2 py-1 md:mx-0 ${availability === "availability.open" ? "border-green-300 bg-green-100" : "border-red-300 bg-red-100"}`}
+    >
+      <div
+        className={`inline-flex ${availability === "availability.open" ? "bg-green-500" : "bg-red-500"} w-2 h-2 rounded-full animate-pulse`}
+      />
+      <span className="text-xs sm:text-sm font-medium">{t(availability)}</span>
     </div>
   );
 }
