8847dcb29ccb20a2bd60c7b52f6d7ebcfb2bbd1c
Makene Neto <makeneto17@gmail.com>
2026-09-12 11:53:54 +0100

    fix: remover id do cabeçalho no componente HomeHero

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -5,10 +5,7 @@ import HeroContent from "./ui/hero-content";
 
 export default function HomeHero() {
   return (
-    <header
-      id="home-hero"
-      className="-mt-35 relative flex min-h-screen w-full items-center justify-center overflow-hidden md:justify-start"
-    >
+    <header className="md:w-5xl 2xl:w-7xl mx-auto lg:-mt-55 2xl:-mt-35 relative flex min-h-screen w-full items-center justify-center overflow-hidden md:justify-start">
       <div
         className="sm:mt-45 absolute inset-x-0 bottom-0 h-[48%] bg-[url('/hero-image-mobile.png')] bg-position-[35%_bottom] bg-no-repeat md:inset-0 md:h-auto md:bg-[url('/hero-image.png')] md:bg-position-[right_center]"
         style={{
