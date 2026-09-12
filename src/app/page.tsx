e133a0b525bfce02d15a797a2255cc002fc712c0
Makene Neto <makeneto17@gmail.com>
2026-09-12 11:53:37 +0100

    refactor: remover padding horizontal em main na página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Service from "@/components/Service";
 
 export default function HomePage() {
   return (
-    <main className="bg-dotted mt-30 sm:mt-35 xl:mt-40 2xl:mt-10 w-full pb-12 sm:px-8 2xl:px-0 ">
+    <main className="bg-dotted mt-30 sm:mt-35 xl:mt-40 2xl:mt-10 w-full pb-12 2xl:px-0 ">
       <div>
         <HomeHero />
         <Numbers />
