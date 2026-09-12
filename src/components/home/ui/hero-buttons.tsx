e42d5f2b938ac6ae20c2ddd0fccf13e0cdb35b82
Makene Neto <makeneto17@gmail.com>
2026-09-12 11:40:29 +0100

    refactor: substituir botões por links no componente HeroButtons e adicionar tradução

--- src/components/home/ui/hero-buttons.tsx
+++ src/components/home/ui/hero-buttons.tsx
@@ -1,24 +1,25 @@
+import { ArrowDown, ArrowUp } from "lucide-react";
 import Link from "next/link";
-
-import { Button } from "@/components/ui/button";
+import { useTranslation } from "react-i18next";
 
 export default function HeroButtons() {
-  return (
-    <div className="flex items-center gap-2 sm:flex-row">
-      <Button
-        size="lg"
-        className="cursor-pointer mt-6 rounded-lg bg-sky-500 dark:bg-sky-950 px-4 py-1.5 text-white transition-colors hover:bg-sky-600 hover:dark:bg-sky-800"
-      >
-        Ver produtos
-      </Button>
+  const { t } = useTranslation();
 
-      <Button
-        size="lg"
-        variant="ghost"
-        className="cursor-pointer mt-6 rounded-lg px-4 py-1.5 text-black dark:text-white transition-colors"
+  return (
+    <div className="mt-12 flex items-center justify-center gap-2 md:justify-start">
+      <Link
+        href="/products"
+        className="flex items-center gap-2 rounded-full bg-sky-500 px-4.5 py-2 sm:py-2.5 text-sm font-semibold text-white hover:bg-sky-600 transition"
       >
-        <Link href="#benefits">Ver Serviços</Link>
-      </Button>
+        {t("header.primaryBtn")} <ArrowUp className="w-4 h-4" />
+      </Link>
+
+      <Link
+        href="#services"
+        className="flex items-center gap-2 rounded-full bg-white px-4.5 py-2 sm:py-2.5 text-sm font-semibold text-zinc-900 border border-transparent hover:border-zinc-200 hover:bg-zinc-50 transition"
+      >
+        {t("header.secondaryBtn")} <ArrowDown className="w-4 h-4" />
+      </Link>
     </div>
   );
 }
