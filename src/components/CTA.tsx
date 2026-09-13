884dc5a61b3944fb6be5ebf5aac03e5c192ec2ff
Makene Neto <makeneto17@gmail.com>
2026-09-13 16:54:26 +0100

    fix: ajustar formatação e remover ponto e vírgula desnecessário no componente CTA

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,16 +1,17 @@
-"use client";
+"use client"
 
-import { useTranslation } from "react-i18next";
-import Logo from "./Logo";
+import { useTranslation } from "react-i18next"
+import Logo from "./Logo"
 
 export default function CTA() {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
   return (
-    <section className="mx-4 mt-55 -mb-30 md:w-5xl 2xl:w-7xl sm:mx-auto bg-blue-500 rounded-3xl py-12 2xl:py-20 shadow-[0_20px_30px_rgba(15,23,42,0.08)] grid justify-center">
+    <section className="mx-4 sm:mx-8 mt-55 -mb-30 lg:w-5xl 2xl:w-7xl lg:mx-auto bg-blue-500 rounded-3xl py-12 2xl:py-20 shadow-[0_20px_30px_rgba(15,23,42,0.08)] grid justify-center">
       <Logo className="mx-auto mb-8" variant="light" />
+
       <div className="max-w-2xl mx-auto text-center">
-        <h1 className="text-white font-semibold xl:text-2xl 2xl:text-[1.8rem] mb-5">
+        <h1 className="text-white font-semibold lg:text-2xl 2xl:text-[1.8rem] mb-5">
           {t("cta.title")}
         </h1>
 
@@ -22,5 +23,5 @@ export default function CTA() {
         </button>
       </div>
     </section>
-  );
+  )
 }
