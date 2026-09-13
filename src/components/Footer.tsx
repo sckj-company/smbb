569487b4c44859f0b7abdca3f8868a6f96804917
Makene Neto <makeneto17@gmail.com>
2026-09-13 16:55:00 +0100

    fix: ajustar formatação e classes no componente Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,8 @@ export default function Footer() {
   const currentYear = now.getFullYear();
 
   return (
-    <footer className="md:w-5xl 2xl:w-7xl mx-auto mt-30 sm:mt-50 grid xl:flex xl:justify-between gap-15 pb-4 xl:pb-8 px-4 sm:px-0">
-      <div className="grid gap-5 xl:w-120">
+    <footer className="lg:w-5xl 2xl:w-7xl mx-auto mt-30 sm:mt-50 grid lg:flex lg:justify-between gap-15 pb-4 sm:pb-8 xl:pb-8 px-4 sm:px-8 lg:px-0">
+      <div className="grid gap-5 lg:w-120">
         <Logo />
         <p className="text-sm 2xl:text-base text-gray-600">
           {t("footer.description")}
