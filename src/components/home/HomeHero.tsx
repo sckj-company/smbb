5c9d443ff4718128731ee29f7fb6530b8131e616
Makene Neto <makeneto17@gmail.com>
2026-09-13 14:15:38 +0100

    fix: atualizar imagens de fundo do componente HomeHero para formato WebP

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -7,7 +7,7 @@ export default function HomeHero() {
   return (
     <header className="md:w-5xl 2xl:w-7xl mx-auto lg:-mt-55 2xl:-mt-35 relative flex min-h-screen w-full items-center justify-center overflow-hidden md:justify-start">
       <div
-        className="sm:mt-45 absolute inset-x-0 bottom-0 h-[48%] bg-[url('/hero-image-mobile.png')] bg-position-[35%_bottom] bg-no-repeat md:inset-0 md:h-auto md:bg-[url('/hero-image.png')] md:bg-position-[right_center]"
+        className="sm:mt-45 absolute inset-x-0 bottom-0 h-[48%] bg-[url('/hero-image-mobile.webp')] bg-position-[35%_bottom] bg-no-repeat md:inset-0 md:h-auto md:bg-[url('/hero-image.webp')] md:bg-position-[right_center]"
         style={{
           backgroundSize: "contain"
         }}
