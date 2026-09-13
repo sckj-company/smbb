d916ce14ad0f3b98f252b931d2fcf8168a144011
Makene Neto <makeneto17@gmail.com>
2026-09-13 16:58:44 +0100

    fix: ajustar formatação e lógica de renderização no componente HomeHero

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -5,15 +5,15 @@ import HeroContent from "./ui/hero-content"
 
 export default function HomeHero() {
   return (
-    <header className="md:w-5xl 2xl:w-7xl mx-auto lg:-mt-55 2xl:-mt-35 relative flex min-h-screen w-full items-center justify-center overflow-hidden md:justify-start">
+    <header className="lg:w-5xl 2xl:w-7xl mx-auto lg:-mt-55 2xl:-mt-35 relative flex min-h-screen w-full items-center justify-center overflow-hidden lg:justify-start">
       <div
-        className="sm:mt-45 absolute inset-x-0 bottom-0 h-[48%] bg-[url('/hero-image-mobile.webp')] bg-position-[35%_bottom] bg-no-repeat md:inset-0 md:h-auto md:bg-[url('/hero-image.webp')] md:bg-position-[right_center]"
+        className="lg:mt-55 absolute inset-x-0 bottom-0 h-[48%] sm:h-[65%] lg:h-auto bg-[url('/hero-image-mobile.webp')] bg-position-[35%_bottom] bg-no-repeat lg:inset-0 lg:bg-[url('/hero-image.webp')] lg:bg-position-[right_center]"
         style={{
           backgroundSize: "contain",
         }}
       />
 
-      <div className="relative z-10 mx-auto flex w-full max-w-7xl flex-col items-center px-4 pb-[42%] text-center md:items-start md:px-0 md:pb-0 md:text-left">
+      <div className="relative z-10 mx-auto flex w-full max-w-7xl flex-col items-center px-4 pb-[42%] text-center lg:items-start lg:px-0 lg:pb-0 lg:text-left">
         <HeroContent />
         <HeroButtons />
       </div>
