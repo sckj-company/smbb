6a1fcbaf16f8d28658a248f1174ddcbed87a7150
Makene Neto <makeneto17@gmail.com>
2026-09-24 17:02:31 +0100

    style: aumentar o padding bottom em dispositivos móveis

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
   const currentYear = now.getFullYear();
 
   return (
-    <footer className="lg:w-5xl 2xl:w-7xl mx-auto mt-30 sm:mt-50 grid lg:flex lg:justify-between gap-15 pb-4 sm:pb-8 xl:pb-8 px-4 sm:px-8 lg:px-0">
+    <footer className="lg:w-5xl 2xl:w-7xl mx-auto mt-30 sm:mt-50 grid lg:flex lg:justify-between gap-15 pb-35 sm:pb-8 xl:pb-8 px-4 sm:px-8 lg:px-0">
       <div className="grid gap-5 lg:w-120">
         <Logo />
         <p className="text-sm 2xl:text-base text-gray-600">
